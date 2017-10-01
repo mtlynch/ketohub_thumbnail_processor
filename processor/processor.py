916e550a882897867362c7efca7ed1e3c4b85ef7
Michael Lynch <michael@ketohub.io>
2017-09-30 23:50:30 -0400

    Fixing formatting

--- processor/processor.py
+++ processor/processor.py
@@ -5,6 +5,7 @@ import os
 
 from PIL import Image
 
+
 def dummy():
     pass
 
@@ -14,15 +15,17 @@ def main(args):
         os.makedirs(args.output_root)
     for recipe_key in os.listdir(args.input_root):
         image_path = os.path.join(args.input_root, recipe_key, 'main.jpg')
-        resized_path = os.path.join(args.output_root, recipe_key + '_thumbnail.jpg')
+        resized_path = os.path.join(args.output_root,
+                                    recipe_key + '_thumbnail.jpg')
         image = Image.open(image_path)
         image.thumbnail((340, 230))
         image.convert('RGB').save(resized_path, "JPEG")
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(prog='KetoHub Thumbnail Processor',
-            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser = argparse.ArgumentParser(
+        prog='KetoHub Thumbnail Processor',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-i', '--input_root')
     parser.add_argument('-o', '--output_root')
     main(parser.parse_args())
