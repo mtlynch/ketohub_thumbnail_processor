db568afac92c427e4d2e0dd0fdea7981d8fa19e0
Michael Lynch <mtlynch@users.noreply.github.com>
2019-04-23 03:09:08 -0500

    Upgrading dev packages (#21)
    * Upgrading dev packages
    
    * Adding a pip upgrade step to Travis
    
    * Changing to pylint 1.9.4, the last available for Python2
    
    * Fixing whitespace

--- tests/test_resize_job.py
+++ tests/test_resize_job.py
@@ -6,38 +6,33 @@ from processor import resize_job
 class JobTest(unittest.TestCase):
 
     def test_initializes_properties(self):
-        job = resize_job.Job(
-            '/foo/original_path.jpg',
-            '/bar/resized_path-400w.jpg',
-            new_width=400,
-            new_height=200)
+        job = resize_job.Job('/foo/original_path.jpg',
+                             '/bar/resized_path-400w.jpg',
+                             new_width=400,
+                             new_height=200)
         self.assertEqual('/foo/original_path.jpg', job.raw_path())
         self.assertEqual('/bar/resized_path-400w.jpg', job.resized_path())
         self.assertEqual(400, job.new_width())
         self.assertEqual(200, job.new_height())
 
     def test_equal_jobs_are_equal(self):
-        job_a = resize_job.Job(
-            '/foo/original_path.jpg',
-            '/bar/resized_path-400w.jpg',
-            new_width=400,
-            new_height=200)
-        job_b = resize_job.Job(
-            '/foo/original_path.jpg',
-            '/bar/resized_path-400w.jpg',
-            new_width=400,
-            new_height=200)
+        job_a = resize_job.Job('/foo/original_path.jpg',
+                               '/bar/resized_path-400w.jpg',
+                               new_width=400,
+                               new_height=200)
+        job_b = resize_job.Job('/foo/original_path.jpg',
+                               '/bar/resized_path-400w.jpg',
+                               new_width=400,
+                               new_height=200)
         self.assertEqual(job_a, job_b)
 
     def test_unequal_jobs_are_not_equal(self):
-        job_a = resize_job.Job(
-            '/foo/original_path.jpg',
-            '/bar/resized_path-400w.jpg',
-            new_width=400,
-            new_height=200)
-        job_b = resize_job.Job(
-            '/fribby/boo.jpg',
-            '/frobby/bing-400w.jpg',
-            new_width=800,
-            new_height=600)
+        job_a = resize_job.Job('/foo/original_path.jpg',
+                               '/bar/resized_path-400w.jpg',
+                               new_width=400,
+                               new_height=200)
+        job_b = resize_job.Job('/fribby/boo.jpg',
+                               '/frobby/bing-400w.jpg',
+                               new_width=800,
+                               new_height=600)
         self.assertNotEqual(job_a, job_b)
